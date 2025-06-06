6a26dd1e80d5a6ea3e4dcef401a37f0442651a1d
sugarflowers <128127805+sugarflowers@users.noreply.github.com>
2025-06-06 17:29:29 +0900

    add functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::fs;
 #[allow(unused_imports)]
-use serde_json::{Value, json};
+use serde_json::{Value, json, Map};
+
 
 #[derive(Debug)]
 pub struct JsonData {
@@ -16,29 +17,47 @@ impl JsonData {
         }
     }
 
+    pub fn object(value:Value) -> Self {
+        Self {
+            json_data: value,
+        }
+    }
 
     pub fn get_string(&self, key: &str) -> String {
-        self.json_data.get(key).and_then(Value::as_str).unwrap_or("").to_string()
+        self.json_data.get(key).and_then(Value::as_str).expect("is not string").to_string()
     }
 
     pub fn get_i64(&self, key: &str) -> i64 {
-        self.json_data.get(key).and_then(Value::as_i64).unwrap_or(0) 
+        self.json_data.get(key).and_then(Value::as_i64).expect("is not number")
     }
 
     pub fn get_f64(&self, key: &str) -> f64 {
-        self.json_data.get(key).and_then(Value::as_f64).unwrap_or(0.0)
+        self.json_data.get(key).and_then(Value::as_f64).expect("is not number")
     }
 
     pub fn get_bool(&self, key: &str) -> bool {
-        self.json_data.get(key).and_then(Value::as_bool).unwrap_or(false)
+        self.json_data.get(key).and_then(Value::as_bool).expect("is not bool")
     }
 
+    pub fn get_object(&self, key: &str) -> JsonData {
+        JsonData::object(json!(self.json_data.get(key).and_then(Value::as_object).expect("is not object")))
+    }
+
+
+    pub fn get_array(&self, key: &str) -> Vec<Value> {
+        self.json_data.get(key).and_then(Value::as_array).expect("is not array").to_vec()
+    }
+
+    pub fn get_array_index(&self, key: &str, index: usize) -> Value {
+        JsonData::get_array(&self, key)[index].clone()
+    }
 
     pub fn set(&mut self, key: &str, value: Value) {
         if let Some(obj) = self.json_data.as_object_mut() {
             obj.insert(key.to_string(), value);
         }
     }
+
 
 
     pub fn save(&self, filename: &str) {
@@ -56,8 +75,16 @@ test.json
 {
     "name": "hanako",
     "age": 23,
-    "single": true
+    "single": true,
+    "sub_data": {
+        "address": "tokyo",
+        "group": ["a", "b"]
+    }
 }
+
+$ cargo test -- --nocapture
+
+
 */
 
 #[test]
@@ -65,6 +92,19 @@ fn json_test() {
     let mut jd = JsonData::open("test.json");
     println!("{:?}", jd);
 
+
+    //let mut su = JsonData::object(jd.get_object("sub_data"));
+    let mut su = jd.get_object("sub_data");
+    println!("{:?}", su);
+
+    println!("address: {}", su.get_string("address"));
+
+
+    let ar = su.get_array("group");
+    println!("{:?}", ar);
+    println!("{}", su.get_array_index("group", 0).as_str().unwrap());
+
+    /*
     let name = jd.get_string("name");
     println!("name: {}", name);
     let single = jd.get_bool("single");
@@ -80,5 +120,6 @@ fn json_test() {
     println!("{:?}", jd);
 
     jd.save("test2.json");
+    */
 }
 
