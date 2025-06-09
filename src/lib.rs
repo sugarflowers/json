f6847d13292409aa63fe8bccb851bdd64d9fd8ac
sugarflowers <128127805+sugarflowers@users.noreply.github.com>
2025-06-09 11:51:49 +0900

    modify functions

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,17 @@ use std::fs;
 use serde_json::{Value, json, Map};
 
 
-#[derive(Debug)]
+#[derive(Default, Debug)]
 pub struct JsonData {
     pub json_data: Value,
 }
 
 impl JsonData {
-    pub fn open(filepath: &str) -> Self {
+    pub fn new() -> JsonData {
+        JsonData::default()
+    }
+
+    pub fn open(filepath: &str) -> JsonData {
     let data = fs::read_to_string(filepath).expect("failed to read json file");
         let json_data:Value = serde_json::from_str(&data).expect("failed to parse JSON");
         Self {
@@ -23,33 +27,12 @@ impl JsonData {
         }
     }
 
-    pub fn get_string(&self, key: &str) -> String {
-        self.json_data.get(key).and_then(Value::as_str).expect("is not string").to_string()
+    pub fn get(&self, key: &str) -> Option<&Value> {
+        self.json_data.get(key)
     }
 
-    pub fn get_i64(&self, key: &str) -> i64 {
-        self.json_data.get(key).and_then(Value::as_i64).expect("is not number")
-    }
-
-    pub fn get_f64(&self, key: &str) -> f64 {
-        self.json_data.get(key).and_then(Value::as_f64).expect("is not number")
-    }
-
-    pub fn get_bool(&self, key: &str) -> bool {
-        self.json_data.get(key).and_then(Value::as_bool).expect("is not bool")
-    }
-
-    pub fn get_object(&self, key: &str) -> JsonData {
-        JsonData::object(json!(self.json_data.get(key).and_then(Value::as_object).expect("is not object")))
-    }
-
-
-    pub fn get_array(&self, key: &str) -> Vec<Value> {
-        self.json_data.get(key).and_then(Value::as_array).expect("is not array").to_vec()
-    }
-
-    pub fn get_array_index(&self, key: &str, index: usize) -> Value {
-        JsonData::get_array(&self, key)[index].clone()
+    pub fn values(&self) -> &Value {
+        &self.json_data
     }
 
     pub fn set(&mut self, key: &str, value: Value) {
@@ -58,8 +41,6 @@ impl JsonData {
         }
     }
 
-
-
     pub fn save(&self, filename: &str) {
         let json_str = serde_json::to_string_pretty(&self.json_data)
             .expect("failed to serialize json");
@@ -67,6 +48,35 @@ impl JsonData {
     }
 }
 
+trait OptionValueExt {
+    fn as_str(&self) -> String;
+    fn as_i64(&self) -> i64;
+    fn as_f64(&self) -> f64;
+    fn as_bool(&self) -> bool;
+    fn as_obj(&self) -> JsonData;
+    fn as_array(&self) -> Vec<Value>;
+}
+
+impl OptionValueExt for Option<&Value> {
+    fn as_str(&self) -> String {
+        self.and_then(Value::as_str).expect("is not string").to_string()
+    }
+    fn as_i64(&self) -> i64 {
+        self.and_then(Value::as_i64).expect("is not number")
+    }
+    fn as_f64(&self) -> f64 {
+        self.and_then(Value::as_f64).expect("is not number")
+    }
+    fn as_bool(&self) -> bool {
+        self.and_then(Value::as_bool).expect("is not bool")
+    }
+    fn as_obj(&self) -> JsonData {
+        JsonData::object(json!(self.and_then(Value::as_object).expect("is not object")))
+    }
+    fn as_array(&self) -> Vec<Value> {
+        self.and_then(Value::as_array).expect("is not array").to_vec()
+    }
+}
 
 /*
 
@@ -91,35 +101,29 @@ $ cargo test -- --nocapture
 fn json_test() {
     let mut jd = JsonData::open("test.json");
     println!("{:?}", jd);
-
-
-    //let mut su = JsonData::object(jd.get_object("sub_data"));
-    let mut su = jd.get_object("sub_data");
-    println!("{:?}", su);
-
-    println!("address: {}", su.get_string("address"));
-
-
-    let ar = su.get_array("group");
-    println!("{:?}", ar);
-    println!("{}", su.get_array_index("group", 0).as_str().unwrap());
-
-    /*
-    let name = jd.get_string("name");
-    println!("name: {}", name);
-    let single = jd.get_bool("single");
-    println!("single: {:?}", single);
-
-
-    let age:i64 = 24;
-    jd.set("age", json!(age));
-    jd.set("place", json!("東京".to_string()));
-    //jd.set("age", Value::Number(age.into()));
-    //jd.set("place", Value::String("東京".to_string()));
     
+    println!("{}", jd.get("name").as_str());
+    println!("{}", jd.get("age").as_i64()); 
+    println!("{}", jd.get("single").as_bool());
+
+    println!("{}", jd.get("sub_data").as_obj().get("address").as_str());
+
+
+
+    let mut sd = jd.get("sub_data").as_obj();
+
+    println!("{}", sd.get("group").as_array()[0].as_str().unwrap());
+
+    let g0_w = sd.get("group").as_array();
+    let g0 = g0_w[0].as_str().unwrap();
+    println!("{}", g0);
+
+    sd.set("address", json!("osaka".to_string()));
+    sd.set("place", json!("midousuji".to_string()));
+    jd.set("sub_data", json!(sd.values()));
     println!("{:?}", jd);
 
     jd.save("test2.json");
-    */
+
 }
 
